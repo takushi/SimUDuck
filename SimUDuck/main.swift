e9fce91684e534fa1866ce523eabe34b1b44060e
takushi <takushi@mfmf.me>
2015-07-11 15:12:33 +0900

    Initial Commit

---
+++ SimUDuck/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SimUDuck
+//
+//  Created by Homma Takushi on 2015/07/11.
+//  Copyright (c) 2015年 mfmf.me. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
